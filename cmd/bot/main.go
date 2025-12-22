3e4d5aaa40b6e3acaeded8f113ad32933280417f
Shubh Arya <shubh.arya37@gmail.com>
2025-12-22 15:14:56 +0530

    initial commit

---
+++ cmd/bot/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Println("LinkedIn Ghostwriter Bot Starting...")
+	log.Println("Hello from Go!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shubh-37/linkedin-ghostwriter
+
+go 1.25.5
